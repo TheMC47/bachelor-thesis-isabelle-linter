7e6eb0182bb5caa952a0f901cf93f8abf455c101
Fabian Huch <huch@in.tum.de>
2021-03-02 15:45:36 +0100

    feat(build): Added auto-compile jars

--- project/IsabellePlugin.scala
+++ project/IsabellePlugin.scala
@@ -10,36 +10,45 @@ import sbt.complete.DefaultParsers._
 object IsabellePlugin extends AutoPlugin {
   val USER_HOME = "USER_HOME"
 
-  private def runIsabelle(bin: File, root: File, cmd: Seq[String], home: File, logger: Logger): Unit = {
-    val process = Process(bin.getAbsolutePath +: cmd, root, USER_HOME -> home.getAbsolutePath).run(logger)
+  private def runIsabelle(bin: File, root: File, cmd: Seq[String], logger: Logger): Unit = {
+    val process = Process(bin.getAbsolutePath +: cmd, root, USER_HOME -> root.getAbsolutePath).run(logger)
     val resultCode = process.exitValue()
+    // Throw exception to trigger build breaks and the such
     if (resultCode != 0) {
       throw new IllegalStateException(s"Running Isabelle command failed with exit code $resultCode")
     }
   }
 
-  override def projectSettings: Seq[Def.Setting[_]] =
+  override def projectSettings: Seq[Def.Setting[_]] = {
     Seq(
       publish / skip := true,
       compile / skip := true,
-      unmanagedJars in Compile ++= (baseDirectory.value / "lib" / "classes" ** "*.jar").get(),
       libraryDependencies += "org.tukaani" % "xz" % "1.8",
       scalaSource in Compile := baseDirectory.value / "src",
+      unmanagedJars in Compile ++= {
+        val isabelleExecutable = baseDirectory.value / "bin" / "isabelle"
+        val projectDir = baseDirectory.value / ".."
+        val logger = streams.value.log
+
+        // Get components and build jars
+        runIsabelle(isabelleExecutable, projectDir, Seq("components", "-a"), logger)
+        runIsabelle(isabelleExecutable, projectDir, Seq("jedit", "-b"), logger)
+
+        // Return jar
+        (baseDirectory.value / "lib" / "classes" ** "*.jar").get()
+      },
       run := {
         val isabelleExecutable = baseDirectory.value / "bin" / "isabelle"
         val projectDir = baseDirectory.value / ".."
-
-        // Run isabelle process
         val logger = streams.value.log
-
-        // Prepare Isabelle instance if necessary
-        runIsabelle(isabelleExecutable, projectDir, Seq("components", "-a"), projectDir, logger)
 
         // Parse user invocation
         val args = spaceDelimited("<arg>").parsed
         logger.info("Running isabelle " + args.mkString(" "))
 
-        runIsabelle(isabelleExecutable, projectDir, args, projectDir, logger)
+        // Run
+        runIsabelle(isabelleExecutable, projectDir, args, logger)
       }
     )
+  }
 }
