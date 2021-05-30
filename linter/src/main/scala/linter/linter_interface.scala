c98072732c1e15ee9d5745d97d5a3df1eebce9a1
Yecine Megdiche <yecine.megdiche@gmail.com>
2021-05-30 11:53:07 +0200

    Added the linter interface

---
+++ linter/src/main/scala/linter/linter_interface.scala
@@ -0,0 +1,41 @@
+package linter
+
+import isabelle._
+
+class Linter_Interface {
+
+  var lint_cache: Map[Document.Node.Name, (Document.Version, List[Linter.Lint_Result])] = Map.empty
+
+  private def update_cache(snapshot: Document.Snapshot): Unit = {
+    lazy val new_cache =
+      lint_cache + (snapshot.node_name -> (snapshot.version, Linter.lint(snapshot, Linter.all_lints)))
+    lint_cache get snapshot.node_name match {
+      case None               => lint_cache = new_cache
+      case Some((version, _)) => if (snapshot.version.id < version.id) lint_cache = new_cache
+    }
+  }
+
+  def lint_results(snapshot: Document.Snapshot): List[Linter.Lint_Result] = {
+    update_cache(snapshot)
+    lint_cache(snapshot.node_name)._2
+  }
+
+  def lint_ranges(snapshot: Document.Snapshot): List[Text.Range] =
+    lint_results(snapshot).map(_.range)
+
+}
+
+class Linter_Variable {
+  private val no_linter: Option[Linter_Interface] = None
+  private var current_linter: Option[Linter_Interface] = no_linter
+
+  def get: Option[Linter_Interface] = synchronized { current_linter }
+
+  def update(options: Options): Unit = synchronized {
+    if (options.bool("linter")) {
+      if (current_linter.isEmpty) {
+        current_linter = Some(new Linter_Interface)
+      }
+    } else current_linter = no_linter
+  }
+}
