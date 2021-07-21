7455037ee5ee6cd8d4abc9b414a75b51e29827c7
Yecine Megdiche <yecine.megdiche@gmail.com>
2021-07-21 11:48:31 +0200

    Parametrize reporting

--- linter/src/main/scala/linter/linter_tool.scala
+++ linter/src/main/scala/linter/linter_tool.scala
@@ -2,46 +2,108 @@ package linter
 
 import isabelle._
 import scala.collection.immutable
+import scala.collection.mutable.ListBuffer
 
 object Linter_Tool {
 
-  def lint(
-      options: Options,
-      logic: String,
-      progress: Progress = new Progress,
-      log: Logger = No_Logger,
-      dirs: List[Path] = Nil,
-      select_dirs: List[Path] = Nil,
-      selection: Sessions.Selection = Sessions.Selection.empty
-  ) {
+  abstract class Lint_CLI[A] {
 
-    // Use Dump to get a context...
-    val context =
-      Dump.Context(
-        options,
-        progress = progress,
-        dirs = dirs,
-        select_dirs = select_dirs,
-        selection = selection,
-        skip_base = true
+    def get_linter_variable: Linter_Variable[A]
+
+    def process_args(
+        linter: Linter_Interface[A],
+        args: Dump.Args,
+        progress: Progress
+    ): Unit = ()
+
+    def process_end(
+        progress: Progress
+    ): Unit = ()
+
+    def apply(
+        options: Options,
+        logic: String,
+        progress: Progress = new Progress,
+        log: Logger = No_Logger,
+        dirs: List[Path] = Nil,
+        select_dirs: List[Path] = Nil,
+        selection: Sessions.Selection = Sessions.Selection.empty
+    ): Unit = {
+      // Use Dump to get a context...
+      val context =
+        Dump.Context(
+          options,
+          progress = progress,
+          dirs = dirs,
+          select_dirs = select_dirs,
+          selection = selection,
+          skip_base = true
+        )
+
+      val linter_variable = get_linter_variable
+      linter_variable.update(options + "linter=true")
+
+      val linter = linter_variable.get.get
+
+      context.build_logic(logic)
+      context
+        .sessions(logic, log = log)
+        .foreach(_.process((args: Dump.Args) => {
+          progress.echo("Processing theory " + args.print_node + " ...")
+          process_args(linter, args, progress)
+        }))
+      context.check_errors
+      process_end(progress)
+    }
+  }
+
+  class Lint_JSON extends Lint_CLI[JSON.T] {
+
+    val reports = new ListBuffer[JSON.T]()
+
+    def get_linter_variable: Linter_Variable[JSON.T] =
+      new Linter_Variable(JSON_Reporter, cache = false)
+
+    override def process_args(
+        linter: Linter_Interface[JSON.T],
+        args: Dump.Args,
+        progress: Progress
+    ): Unit = {
+      val start_date = Date.now()
+      val report = linter.report_for_snapshot(args.snapshot)
+      val end_date = Date.now()
+      val timing = end_date.time - start_date.time
+      reports += JSON.Object(
+        "theory" -> args.print_node,
+        "report" -> report,
+        "timing" -> timing.ms
       )
+    }
 
-    val linter_variable = new Linter_Variable(Text_Reporter, cache = false)
-    linter_variable.update(options + "linter=true")
+    override def process_end(
+        progress: Progress
+    ): Unit = {
+      progress.echo(
+        JSON.Format(
+          JSON.Object(
+            "reports" -> reports.toList
+          )
+        )
+      )
+    }
+  }
 
-    val linter = linter_variable.get.get
+  object Lint_Text extends Lint_CLI[String] {
+    override def get_linter_variable: Linter_Variable[String] =
+      new Linter_Variable(Text_Reporter, cache = false)
 
-    context.build_logic(logic)
-    context
-      .sessions(logic, log = log)
-      .foreach(_.process((args: Dump.Args) => {
-        progress.echo("Processing theory " + args.print_node + " ...")
-        val snapshot = args.snapshot
-        val report = linter.report_for_snapshot(snapshot)
-        progress.echo(report)
-      }))
+    override def process_args(
+        linter: Linter_Interface[String],
+        args: Dump.Args,
+        progress: Progress
+    ): Unit =
+      progress.echo(linter.report_for_snapshot(args.snapshot))
 
-    context.check_errors
   }
 
   /* Isabelle tool wrapper */
@@ -63,6 +125,7 @@ object Linter_Tool {
         var options = Options.init()
         var verbose = false
         var exclude_sessions: List[String] = Nil
+        var mode: String = "text"
 
         val getopts = Getopts(
           """
@@ -80,6 +143,7 @@ Usage: isabelle lint [OPTIONS] [SESSIONS ...]
     -o OPTION    override Isabelle system OPTION (via NAME=VAL or NAME)
     -v           verbose
     -x NAME      exclude session NAME and all descendants
+    -r MODE      how to report results (either "text" or "json", default "text")
 
   Lint isabelle theories.
 """,
@@ -93,10 +157,18 @@ Usage: isabelle lint [OPTIONS] [SESSIONS ...]
           "g:" -> (arg => session_groups = session_groups ::: List(arg)),
           "o:" -> (arg => options = options + arg),
           "v" -> (_ => verbose = true),
-          "x:" -> (arg => exclude_sessions = exclude_sessions ::: List(arg))
+          "x:" -> (arg => exclude_sessions = exclude_sessions ::: List(arg)),
+          "r:" -> (arg => mode = arg)
         )
 
         val sessions = getopts(args)
+
+        val lint = mode match {
+          case "text" => Lint_Text
+          case "json" => new Lint_JSON()
+          case _ => error(s"Unrecognized reporting mode $mode")
+        }
+
         val progress = new Console_Progress(verbose = verbose)
         progress.interrupt_handler {
           lint(
